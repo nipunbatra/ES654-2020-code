9f13dd2810b3e855a7bb0c835410eaeacde8787b
Nipun <nipunbatra0@gmail.com>
2020-01-07 16:32:02 +0530

    added some new files

---
+++ tree/base.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as import pd
+import matplotlib.pyplot as plt
+from utils import entropy, information_gain, gini_index
+
+class Tree():
+    def __init__():
+        pass
+
+    def fit():
+        pass
+
+    def predict():
+        pass
+
+    def plot():
+        pass
+
+    def _split():
+        pass
+
+    
---
+++ utils.py
@@ -0,0 +1,14 @@
+
+
+def entropy(S):
+    """
+    S: np.array of dtype categorical
+    """
+    
+    pass
+
+def gini_index():
+    pass
+
+def information_gain():
+    pass
