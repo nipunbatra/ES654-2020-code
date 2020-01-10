e5e171b0a914fac5dfd65cfd31e8180647e4ea19
Nipun Batra <nipunbatra0@gmail.com>
2020-01-10 12:29:29 +0530

    Create experiments.py

---
+++ experiments.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from tree.base import DecisionTree
+from metrics import *
+
+np.random.seed(42)
+num_average_time = 100
+
+# Learn DTs 
+# ...
+# 
+# Function to calculate average time (and std) taken by fit() and predict() for different N and P for 4 different cases of DTs
+# ...
+# Function to plot the results
+# ..
+# Function to create fake data (take inspiration from usage.py)
+# ...
+# ..other functions
