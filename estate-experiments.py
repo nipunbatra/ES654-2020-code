3183a74a3316c9fc3b35bd04596b79d96685deec
Nipun Batra <nipunbatra0@gmail.com>
2020-01-10 12:26:29 +0530

    Create estate-experiments.py

---
+++ estate-experiments.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tree.base import DecisionTree
+from metrics import *
+
+np.random.seed(42)
+
+# Read real-estate data set
+# ...
+# 
