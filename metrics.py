7b7cacfb2c562d9d2f8c52d8019d7e2d765f92cb
Varun Gohil <gohil.varun@iitgn.ac.in>
2020-01-10 09:32:51 +0530

    Added an example assert

--- metrics.py
+++ metrics.py
@@ -10,6 +10,12 @@ def accuracy(y_hat, y):
     Output:
     > Returns the accuracy as float
     """
+    """
+    The following assert checks if sizes of y_hat and y are equal.
+    Students are required to add appropriate assert checks at places to
+    ensure that the function does not fail in corner cases.
+    """
+    assert(y_hat.size == y.size)
     # TODO: Write here
     pass
 
