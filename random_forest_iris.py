7caa8f3dd37a81742fdda83b0a4bd1bb6f6c8c97
Varun Gohil <gohil.varun@iitgn.ac.in>
2020-01-23 14:23:44 +0530

    Create random_forest_iris.py

---
+++ random_forest_iris.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from metrics import *
+
+from tree.randomForest import RandomForestClassifier
+from tree.randomForest import RandomForestRegressor
+
+###Write code here
